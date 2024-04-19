9930a6fbbd3319233ddc6af50483ce5015b47822
alextolon <atolon@fundacionsafa.es>
2024-04-19 08:12:17 +0200

    Adding Column layout and outsourcing states

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,8 +1,10 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
 import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -11,17 +13,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
+fun buildMsg(txt: String): String = "Hello, $txt"
 @Composable
 @Preview
 fun App() {
-    var text = remember { mutableStateOf("Hello, World!") }
-
+    val text = remember { mutableStateOf("") }
+    val buttonEnabled = text.value.isNotEmpty()
     MaterialTheme {
-        Button(onClick = {
-            text.value = "Hello, Desktop!"
-        }) {
-            Text(text.value)
+        Column {
+            TextField(value = text.value, onValueChange = { newText -> text.value = newText })
+            Text(text = buildMsg(text.value))
+            Button(onClick = { text.value = "" }, enabled = buttonEnabled ) {
+                Text("Clean")
+            }
         }
+
     }
 }
 
