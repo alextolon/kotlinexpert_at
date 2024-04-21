fddf023084b56b95939413adefca79c563c3df51
alextolon <atolon@fundacionsafa.es>
2024-04-21 10:38:46 +0200

    Using Note class and basic UI presentation with JetPack Compose

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,43 +1,63 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Button
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Mic
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 class AppState {
-    val text = mutableStateOf("")
-    fun isButtonEnabled() = text.value.isNotEmpty()
-    /*
-    val buttonEnabled : Boolean
-        get() = text.value.isNotEmpty()
-        Modificando más adelante con enabled = appState.buttonEnabled
-    }
-     */
+    val notes = mutableStateOf(getNotes())
 }
 
 @Composable
 @Preview
 fun App(appState: AppState) {
     MaterialTheme {
-        Column {
-            TextField(value = appState.text.value, onValueChange = { newText -> appState.text.value = newText })
-            Text(text = buildMessage(appState.text.value))
-            Button(onClick = { appState.text.value = "" }, enabled = appState.isButtonEnabled()) {
-                Text("Clean")
+        LazyColumn(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            items(appState.notes.value) { note ->
+                Card(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(0.8f)
+                ) {
+                    Column(
+                        modifier = Modifier.padding(16.dp)
+                    ) {
+                        Row {
+                            Text(
+                                text = note.title,
+                                style = MaterialTheme.typography.h6,
+                                modifier = Modifier.weight(1f)
+                            )
+                            if (note.type == Note.Type.AUDIO) {
+                                Icon(
+                                    imageVector = Icons.Default.Mic,
+                                    contentDescription = null
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(note.description)
+                    }
+                }
             }
         }
 
     }
 }
-
-fun buildMessage(ms: String): String = "Hello $ms"
 
 fun main() {
     //Antes de llamar a Compose
